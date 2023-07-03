0a41105f9ac627375bf2b57879bc712d54f915e5
Полина Николаева <sti-xa@ya.ru>
2023-07-03 21:29:19 +0300

    fix

--- yacut_app/__init__.py
+++ yacut_app/__init__.py
@@ -6,7 +6,7 @@ from settings import Config
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
