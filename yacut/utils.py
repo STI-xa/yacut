73d873afadf67db07134b0f712f734be6791d61f
Полина Николаева <sti-xa@ya.ru>
2023-07-08 13:24:19 +0300

    fix

--- yacut/utils.py
+++ yacut/utils.py
@@ -3,12 +3,12 @@ import string
 
 from . import db
 from .models import URLMap
-from .constants import SHORT_URL_MAX_LENGTH
+# from .constants import SHORT_URL_MAX_LENGTH
 
 
 def get_unique_short_id():
     while True:
         short_id = ''.join(random.choices(
-            string.ascii_letters + string.digits, SHORT_URL_MAX_LENGTH))
+            string.ascii_letters + string.digits, k=6))
         if not db.session.query(db.exists().where(URLMap.short == short_id)).scalar():
             return short_id
